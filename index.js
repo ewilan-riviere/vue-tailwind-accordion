8a84d8bef7b1afccccb98f4b408b4b525688c512
Ewilan <ewilan@dotslashplay.it>
2020-07-27 14:59:30 +0200

    fix

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ import VueClipboard from 'vue-clipboard2'
 import Toasted from 'vue-toasted'
 
 export default {
-  install(Vue, options) {
+  install(Vue, options, isServer) {
     Vue.use(VueClipboard)
     Vue.use(Toasted)
     if (options) {
       // Vue.prototype.$option = options.option
     }
     Vue.component('code-block', VueCodeBlock)
+
+    // to avoid rerror when build cause by vue-toast
+    // to avoid rerror when build cause by vue-toast
+    if (!isServer) {
+      import('vue-toasted' /* webpackChunkName: "notification" */).then(
+        (module) => {
+          Vue.use(module.default)
+        }
+      )
+    }
   },
 }
